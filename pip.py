4e61f4452b0706515f4a86a7fe88822c7e45024e
alien-keric <alienkeric@gmail.com>
2024-04-05 08:06:05 -0400

    Another commit for today for today

--- pip.py
+++ pip.py
@@ -1,7 +1,6 @@
 #!/bin/python3
 
-#initilization
-
+#importing lib
 import os
 import shutil
 
@@ -9,8 +8,8 @@ def separate_frontend_backend(src_dir, frontend_dir, backend_dir):
     """
     Separate frontend and backend files into their respective directories.
     """
-    frontend_extensions = ['.html', '.css', '.js', '.jsx', '.vue', '.ts', '.tsx', '.scss', '.sass', '.less', '.styl', '.svg', '.png', '.jpg', '.gif']
-    backend_extensions = ['.py', '.java', '.rb', '.php', '.cs', '.cpp', '.h', '.m', '.swift', '.go', '.ts', '.js', '.sql', '.pl']
+    frontend_extensions = ['.html', '.css', '.js', '.jsx', '.vue', '.ts', '.tsx', '.scss', '.sass', '.less', '.styl', '.svg', '.png', '.jpg', '.gif'] # we can add more files here
+    backend_extensions = ['.py', '.java', '.rb', '.php', '.cs', '.cpp', '.h', '.m', '.swift', '.go', '.ts', '.js', '.sql', '.pl','.yml','.xml'] # we can add more backend ext here
 
     for root, dirs, files in os.walk(src_dir):
         for file in files:
@@ -21,14 +20,14 @@ def separate_frontend_backend(src_dir, frontend_dir, backend_dir):
 
 def main():
     # specifing the source dir
-    src_dir = '/var/www/html/'  # Change this to your source directory
+    src_dir = '/var/www/' #specify the location
     frontend_dir = 'frontend'
     backend_dir = 'backend'
 
     os.makedirs(frontend_dir, exist_ok=True)
     os.makedirs(backend_dir, exist_ok=True)
 
-    # Separate frontend and backend files
+    # calling a separated_front_backend function here
     separate_frontend_backend(src_dir, frontend_dir, backend_dir)
 
     # Continue with your pipeline stages
